70888654490f96ccd0e1311dabef6f29f7c23634
programmerkoji <60305092+programmerkoji@users.noreply.github.com>
2022-01-29 14:36:47 +0900

    ハンバーガーメニューの実装

---
+++ burger/script.js
@@ -0,0 +1,22 @@
+const burgerBtn = document.querySelector('.bl_burgerBtn');
+const headNav = document.querySelector('.bl_head_gloNav');
+const closeBtn = document.querySelector('.gnav_closeBtn');
+const body = document.querySelector('body');
+
+const toggleMenu = () => {
+  burgerBtn.classList.toggle('active');
+  headNav.classList.toggle('active');
+}
+
+const closeBtnActive = () => {
+  const burgerBtnActive = burgerBtn.classList.contains("active");
+  const headNavActive = burgerBtn.classList.contains("active");
+
+  if (burgerBtnActive || headNavActive) {
+    burgerBtn.classList.remove("active");
+    headNav.classList.remove("active");
+  }
+}
+
+burgerBtn.addEventListener("click", toggleMenu);
+closeBtn.addEventListener("click", closeBtnActive);
